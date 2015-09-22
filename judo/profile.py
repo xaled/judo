e3ff69e3fd9c37a96720379cf724fe9f844c5ccd
xaled <kh.grandi@gmail.com>
2015-09-22 19:18:02 +0100

    new profile ok

--- judo/profile.py
+++ judo/profile.py
@@ -17,8 +17,7 @@ static functions:
 
 classes:
 - Profile
-- ProfileException
-
+- ProfileException # TODO: is it necessary??
 """
 
 
@@ -26,8 +25,8 @@ classes:
 def getAllProfiles(): # TODO: Get all Available profiles:
     """
     Get all available profiles in a profiledir
-        :return: - list<Profiles>
-        :raises: ProfileException in case of io error
+    :return: - list<Profiles>
+    :raises: IOError
     """
     pass
 
@@ -58,12 +57,18 @@ def createProfile(profilename, profile_template=DEFAULT_PROFILE_CONF):
     for d in profile_conf["home_dirs"]:
         mkdirIfNotExist(join(profile_home,d))
     saveProfile(profile_conf, profile_conf_file)
+    return Profile(profilename, profile_conf)
 
 
 def getProfile(profilename):
-    profile = loadProfile(profilename)
-    if profile == None:
-        profile = createProfile(profilename)
+    try:
+        profile = loadProfile(profilename)
+        if profile == None:
+            profile = createProfile(profilename)
+    except IOError, e:
+        logger.error("Error getting profile profilename={profilename}",profilename=profilename,exc_info=True)
+        raise
+
 
     return profile
 
@@ -80,7 +85,7 @@ def loadProfile(profilename):
             - IOError: Access error
     """
     if not RG_PROFILE.match(profilename):
-        raise ValueError("Profile name should be alphanumerical and less than 20 characters",None)
+        raise ValueError("Profile name should be alphanumerical and less than 20 characters")
     profile_conf_file = join(DEFAULT_PROFILE_DIR, profilename + ".json")
     profile_dir = join(DEFAULT_PROFILE_DIR,profilename)
     if isfile(profile_conf_file) and isdir(profile_dir):
@@ -89,9 +94,9 @@ def loadProfile(profilename):
             data = fin.read()
             conf = json.loads(data)
             fin.close()
-            return conf,profile_dir
+            return Profile(profilename,conf,profile_dir)
         except IOError, e:
-            logger.error("Error loading profile conf file",)
+            logger.error("Error loading profile conf file filepath={filepath}",filename=profile_conf_file,exc_info=True)
             raise
             #raise ProfileException("Error loading profile conf file: " + profile_conf_file,e)
     logger.info("Profile not found; profilename=%s", profilename)
@@ -109,9 +114,14 @@ class Profile:
     """
     profilename = ""
     options = dict()
-    profiledir = ""
-    def __init__(self):
-        pass
+    profiledir = None
+    def __init__(self, _pfl_name, _options, _profiledir=None):
+        self.profilename = _pfl_name
+        self.options = _options
+        if _profiledir == None:
+            self.profiledir = join(DEFAULT_PROFILE_DIR, self.profilename)
+        else:
+            self.profiledir = _profiledir
 
 
 
