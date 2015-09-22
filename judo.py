4adb270e8862b1562b5a5e4ede4fc7577cadcd57
xaled <kh.grandi@gmail.com>
2015-09-22 15:59:46 +0100

    io.xml + sync profile progress

--- judo.py
+++ judo.py
@@ -2,8 +2,27 @@ __author__ = 'xaled'
 
 import judo.profile
 import judo.run
+from judo.config import RG_PROFILE
 #import judo.frequent
 #import frequentree.os
+import logging
+import sys
+
+"""
+judo: jailed user do (like sudo but sandbox execution instead)
+
+USAGE: #TODO
+judo  [-u] profile command"
+-u: update profile
+
+Example:  $ judo clean firefox
+"""
+
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger(__name__)
+logger.debug('often makes a very good meal of %s', 'visiting tourists')
+print "test"
+
 
 # CONST
 DEFAULT_CMD = ""
@@ -11,11 +30,29 @@ DEFAULT_PFL = ""
 
 def Main(cmd=DEFAULT_CMD, profile=DEFAULT_PFL, update=False): #other options
     prfl=judo.profile.getProfile(profile)
-    #TODO: check profile if doesn't exist prompt question to create or not?
     rn = judo.run.Run(prfl)
     rn.init()
-    rn.execute() # TODO: blocking or not blocking exec!
-    #TODO: check if there are still other process belonging to jailed uid
-    #TODO: check if execution has exited
+    rn.execute()
     rn.clean()
+
+
+if __name__ == "__main__":
+    #initJU() #TODO: initJUDO & installJUDO scripts (cleaning unvalid locks)
+    if len(sys.argv) <3 or (sys.argv[1]=='-u' and len(sys.argv)< 4):
+        print "usage: judo  [-u] profile command"
+    else:
+        start = 0
+        update = False
+        if sys.argv[1]=='-u':
+            start = 1
+            update = True
+        profile = sys.argv[1+start]
+        cmd= ""
+        for a in sys.argv[2+start:]:
+            cmd += a + " "
+        if not RG_PROFILE.match(profile):
+            print "Profile name should be alphanumerical and less than 20 characters"
+        else:
+            Main(cmd=cmd,profilename=profile,update=update)
+
 
--- judo/profile.py
+++ judo/profile.py
@@ -4,21 +4,49 @@ from config import DEFAULT_PROFILE_CONF, RG_PROFILE,DEFAULT_PROFILE_DIR
 import json
 import logging
 
-
 logger = logging.getLogger(__name__)
 
+"""
+contains Porfile class and static operations on Porfile
+static functions:
+- getProfiles()
+- createProfile(profilename, template=DEFAULT_TEMPLATE)
+- getProfile(profilename)
+- getAllProfiles()
+
+classes:
+- Profile
+- ProfileException
+
+"""
 
 
 
-
-def getProfiles(profiledir=DEFAULT_PROFILE_DIR): # TODO: Get all Available profiles:
+def getAllProfiles(): # TODO: Get all Available profiles:
     """
     Get all available profiles in a profiledir
+        return: - list<Profiles>
+        throws: ProfileException in case of io error
     """
     pass
 
+def saveProfile(profile_conf, profile_conf_file):
+    fou = open(profile_conf_file,"w")
+    fou.write(json.dumps(profile_conf,indent=3))
+    fou.close()
 
 def createProfile(profilename):
+    profile_home = os.path.join(JU_PROFILES_DIR, profilename)
+    profile_conf_file = os.path.join(JU_PROFILES_DIR,profilename+".json")
+    profile_conf = dict(DEFAULT_PROFILE_CONF)
+    profile_conf['name']= profilename
+    mkdirIfNotExist(profile_home)
+    for d in profile_conf["home_dirs"]:
+        mkdirIfNotExist(os.path.join(profile_home,d))
+    saveProfile(profile_conf, profile_conf_file)
+
+
+def getProfile(profilename):
     profile = getProfile(profilename)
     if profile == None:
         profile = createProfile(profilename)
@@ -27,9 +55,18 @@ def createProfile(profilename):
 
 
 
-def getProfile(profilename,profiledir=DEFAULT_PROFILE_DIR):
+def loadsProfile(profilename):
+    """
+    loads profile from json file
+    :param profilename: profile name:
+    :return: - Profile instance
+             - None in case profile not found
+    :throws:
+            - ValuerError: Profile name should be alphanumerical and less than 20 characters
+            - IOError: Access error
+    """
     if not RG_PROFILE.match(profilename):
-        raise ProfilaException("Profile name should be alphanumerical and less than 20 characters",None)
+        raise ValueError("Profile name should be alphanumerical and less than 20 characters",None)
     profile_conf_file = join(profiledir, profilename + ".json")
     profile_dir = join(profiledir,profilename)
     if isfile(profile_conf_file) and isdir(profile_dir):
@@ -39,22 +76,37 @@ def getProfile(profilename,profiledir=DEFAULT_PROFILE_DIR):
             conf = json.loads(data)
             fin.close()
             return conf,profile_dir
-        except Exception, e:
-            raise ProfilaException("Error loading profile conf file: " + profile_conf_file,e)
+        except IOError, e:
+            logger.error("Error loading profile conf file",)
+            raise
+            #raise ProfileException("Error loading profile conf file: " + profile_conf_file,e)
     logger.info("Profile not found; profilename=%s", profilename)
     return None
 
 class Profile:
+    """
+    Profile class:
+        -Attributes:
+            - options: graphic, audio, randomuid, alwaysupdate, homedirs, download_dir, default_update etc...
+            - profilename
+            - profiledir
+        -Methods:
+            -init()
+    """
     profilename = ""
-    profiledir  = ""
-    profileconf = None
+    options = dict()
+    profiledir = ""
     def __init__(self):
         pass
 
 
 
-class ProfilaException(Exception):
+class ProfileException(Exception):
+    """
+    ProfileException class:
+        Gist: exception with message & cause
+    """
     def __init__(self, message, cause):
-        super(ProfilaException, self).__init__(message + ', caused by ' + repr(cause))
+        super(ProfileException, self).__init__(message + ', caused by ' + repr(cause))
         self.cause = cause
 
